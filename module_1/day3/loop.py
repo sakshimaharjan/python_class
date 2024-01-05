095aa49fb4ebae37b58791dfc4cd574d430fe1a4
sakshimaharjan <sakshimaharjan.16310@gmail.com>
2024-01-05 08:57:15 +0545

    prime number

--- module_1/day3/loop.py
+++ module_1/day3/loop.py
@@ -4,8 +4,8 @@
 #prime number
 num = int(input("Enter a number: "))
 count = 0
-for i in range(1,num+1):
-    if(num % i ==0):
+for i in range(1, num+1):
+    if(num % i == 0):
         count += 1
 
 if(count == 2):
