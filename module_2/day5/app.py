cf6caf14aee986c844a76c30841ddcda0b5d0751
sakshimaharjan <sakshimaharjan.16310@gmail.com>
2024-02-18 15:01:16 +0545

    installed module from pip

---
+++ module_2/day5/app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import pandas as pd
+
+st.write("Hello world! How are you?")
+st.image('https://images.unsplash.com/photo-1708103314196-626aa47cc77a?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')
+
+# from flask import flask
+
